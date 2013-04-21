30625be97f4bdc813dd1a3f794f220450506fe9f
maripo <ggVKf4qZ>
2013-04-21 09:26:16 +0900

    Merge preset and random stars

--- web/src/render.js
+++ web/src/render.js
@@ -11,12 +11,14 @@ var prevY = 0;
 var width = 480;
 var height = 480;
 
+var MARGIN_BOTTOM = 80;
+
 function initCanvas(_width, _height) {
 	width = document.body.clientWidth;//_width;
-	height = window.innerHeight - 66;
+	height = window.innerHeight - MARGIN_BOTTOM;
 	editor = new Editor(width, height);
 	editor.initCanvas(width, height);
-	initStars();
+	initStars({preset:SHUTTLE});
     editor.drawStars(0.1);
 }
 
@@ -24,11 +26,20 @@ var STAR_DENSITY = 1/1200;
 function getStarCount () {
 	return Math.floor(width * height * STAR_DENSITY);
 }
-function initStars () {
+function initStars (option) {
 	stars = [];
 	var starCount = getStarCount();
+	var minMagnitude = 0;
+	var maxMagnitude = 6;
+	if (option && option.preset) {
+		minMagnitude = 2;
+		starCount -= option.preset.length;
+		for (var i=0; i<option.preset.length; i++) {
+			stars.push(Star.createPreset(option.preset[i]));
+		}
+	}
 	for (var i=0; i<starCount; i++) {
-		stars.push(Star.createRandom());
+		stars.push(Star.createRandom(minMagnitude, maxMagnitude));
 	}
 }
 function setThreshold() {
@@ -53,7 +64,7 @@ Star.prototype.getColorExpression = function () {
 	return col;
 }
 var COLOR_MINIMUM = 160;
-Star.createRandom = function () {
+Star.createRandom = function (minMagnitude, maxMagnitude) {
 	var rand = Math.random();
 	var r = COLOR_MINIMUM + Math.floor(Math.random() * (255-COLOR_MINIMUM));
 	var g = COLOR_MINIMUM + Math.floor(Math.random() * (255-COLOR_MINIMUM));
@@ -61,9 +72,23 @@ Star.createRandom = function () {
 	return new Star (
 			Math.random() * width,
 			Math.random() * height,
-			(1-Math.pow(rand,6)) * 6.0,
+			minMagnitude + (1-Math.pow(rand,6)) * (maxMagnitude - minMagnitude),
 			{r:r, g:g, b:b});
 };
+Star.createPreset = function (coordinates) {
+	var x = coordinates[0];
+	var y = coordinates[1];
+	//TODO
+	var r = COLOR_MINIMUM + Math.floor(Math.random() * (255-COLOR_MINIMUM));
+	var g = COLOR_MINIMUM + Math.floor(Math.random() * (255-COLOR_MINIMUM));
+	var b = COLOR_MINIMUM + Math.floor(Math.random() * (255-COLOR_MINIMUM));
+	return new Star (
+		x * 10 + 84,
+		y * 10 + 24,
+		0,
+		{r:r, g:g, b:b}
+		);
+}
 function test () {
 	var triangle = new Triangle(
 			[
@@ -110,10 +135,18 @@ Editor.prototype.renderEdge = function (edge) {
 
 Editor.prototype.drawStars = function (brightness) {
 	var threshold = (1-brightness) * 6;
-	var r = Math.floor(brightness*40);
-	var g = Math.floor(brightness*40);
-	var b = Math.floor(brightness*120);
-	canvas.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+	var brightnessTop = brightness;
+	var brightnessBottom = brightness + 0.6;
+	var rTop = Math.floor(brightnessTop*40);
+	var gTop = Math.floor(brightnessTop*40);
+	var bTop = Math.floor(brightnessTop*110);
+	var rBottom = Math.floor(brightnessBottom*40);
+	var gBottom = Math.floor(brightnessBottom*40);
+	var bBottom = Math.floor(brightnessBottom*120);
+	canvas.style.background = "-webkit-linear-gradient(top,"
+		+"rgb("+rTop+","+gTop+","+bTop+"),"
+		+"rgb("+rBottom+","+gBottom+","+bBottom+"))";
+	console.log(canvas.style.background);
 	context.clearRect(0, 0, width, height);
 	var starsToShow = [];
 	for (i=0; i<stars.length; i++) {
